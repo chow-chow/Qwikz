ac805a6c4d8e900f35f83e1815db3cc7c1bbd483
chow-chow <leonardochow@outlook.com>
2024-03-05 20:01:40 -0600

    adding /group routes for: get groups, get group by code, get group by id, update group, delete group and get students

--- backend/controllers/group.py
+++ backend/controllers/group.py
@@ -6,3 +6,34 @@ class GroupController:
   def create_group(data):
     group = GroupService().insert(data)
     return jsonify(group.to_JSON()), 201
+  
+  @staticmethod
+  def get_groups():
+    groups = GroupService().get_all()
+    groups_json = [group.to_JSON() for group in groups]
+    return jsonify(groups_json), 200
+  
+  @staticmethod
+  def get_group_by_code(group_code):
+    group = GroupService().get_by_code(group_code)
+    return jsonify(group.to_JSON()), 200
+  
+  @staticmethod
+  def get_group_by_id(group_id):
+    group = GroupService().get(group_id)
+    return jsonify(group.to_JSON()), 200
+  
+  @staticmethod
+  def delete_group(group_id):
+    return jsonify(GroupService().delete(group_id)), 204
+  
+  @staticmethod
+  def update_group(group_id, data):
+    group = GroupService().update(group_id, data)
+    return jsonify(group.to_JSON()), 200
+  
+  @staticmethod
+  def get_group_students(group_id):
+    students = GroupService().get_students(group_id)
+    students_json = [student.to_JSON() for student in students]
+    return jsonify(students_json), 200
--- backend/routes/group.py
+++ backend/routes/group.py
@@ -7,3 +7,27 @@ group_bp = Blueprint('group', __name__)
 @group_bp.route('/', methods=['POST'])
 def create_group():
   return GroupController.create_group(request.get_json())
+
+@group_bp.route('/', methods=['GET'])
+def get_groups():
+  return GroupController.get_groups()
+
+@group_bp.route('/<string:group_code>', methods=['GET'])
+def get_group_by_code(group_code):
+  return GroupController.get_group_by_code(group_code)
+
+@group_bp.route('/<int:group_id>', methods=['GET'])
+def get_group_by_id(group_id):
+  return GroupController.get_group_by_id(group_id)
+
+@group_bp.route('/<int:group_id>', methods=['DELETE'])
+def delete_group(group_id):
+  return GroupController.delete_group(group_id)
+
+@group_bp.route('/<int:group_id>/', methods=['PUT'])
+def update_group(group_id):
+  return GroupController.update_group(group_id, request.get_json())
+
+@group_bp.route('/<int:group_id>/students', methods=['GET'])
+def get_group_students(group_id):
+  return GroupController.get_group_students(group_id)
