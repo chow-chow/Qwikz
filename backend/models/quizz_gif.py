d82ef4f55a6225a05a5e52ac8415d93a8192db3b
chow-chow <leonardochow@outlook.com>
2024-05-26 00:29:14 -0600

    feat:adding QUIZZ_GIFS table and PL/SQL script to load them into the db

---
+++ backend/models/quizz_gif.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Sequence
+from .. import db
+
+class QUIZZ_GIF(db.Model):
+  """ QuizzGif Model for storing quizz gif related details """
+  _tablename_ = 'QUIZZ_GIF'
+  QUIZZ_GIF_SEQ = Sequence('QUIZZ_GIF_SEQ', metadata=db.metadata)
+
+  # Columns
+  GIF_ID = db.Column(db.Integer, QUIZZ_GIF_SEQ, primary_key=True, server_default=QUIZZ_GIF_SEQ.next_value())
+  GIF_BLOB = db.Column(db.LargeBinary, nullable=False)
+  MIN_SCORE = db.Column(db.Integer, nullable=False)
+  MAX_SCORE = db.Column(db.Integer, nullable=False)
+
+  def to_JSON(self):
+    return {
+      'GIF_ID': self.GIF_ID,
+      'GIF_BLOB': self.GIF_BLOB,
+      'MIN_SCORE': self.MIN_SCORE,
+      'MAX_SCORE': self.MAX_SCORE
+    }
